c0c99df113438c9fe85517f763a1c61a6e2ac5f1
Priscila <lippmanpagliares@gmail.com>
2026-08-20 19:35:38 -0300

    Configura credenciais públicas do Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebas
 // Pegue estes valores em: console.firebase.google.com > seu projeto >
 // ícone de engrenagem > Configurações do projeto > "Seus apps" > SDK setup and configuration.
 const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI",
+  apiKey: "AIzaSyBakz8SBcCT4abpqxekamkbrf8MDlxhRmY",
+  authDomain: "assistente-estudos-lps.firebaseapp.com",
+  projectId: "assistente-estudos-lps",
+  storageBucket: "assistente-estudos-lps.firebasestorage.app",
+  messagingSenderId: "891426240021",
+  appId: "1:891426240021:web:097d480c7ec27e0b9d001c",
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
